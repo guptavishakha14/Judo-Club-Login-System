dd39f693a9df327768af9fd92656fd93dacf4e6d
Vishakha Gupta <59644712+guptavishakha14@users.noreply.github.com>
2022-03-06 14:57:27 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+<title> About Us </title>
+<style>
+body
+{
+      margin: 0;
+      padding: 0;
+      font-family: Century Gothic;
+      background-image: url(about.png);
+      background-size: cover;
+      background-position: center;
+}
+.container
+{
+     height: 75vh;
+     width: 40%;
+     background: rgba(0,0,0,0.7);
+     position: absolute;
+     top: 50%;
+     left: 50%;
+     transform: translate(-50%,-50%);
+     padding: 0 20px;
+}
+h1
+{
+     color: white;
+     margin-top: 30px;
+}
+p
+{
+     color: white;
+     margin-top: 40px;
+     font-size: large;
+     text-indent: 50px;
+     line-height: 32px;
+}
+</style>
+</head>
+<body>
+       <div class="container">
+             <center>
+                 <h1> About Us..</h1>
+                 <p> 
+                     Since 1994, Bajajnagar Judo Club has been finding and featuring some of the best players. We run club offers a range of affordable classes catering for all age groups, from 7 onwards. From coaching  players on the ins-and-outs of the sport to honing their athletic skills, we’re proud to teach not only the love of the game, but to also impart the ideals of good sportsmanship, teamwork and loyalty. We are a friendly and professional club and our doors are open to those wanting to give judo a try for the first time alongside seasoned judoka travelling through the city wanting to visit the dojo for a session on the mats. 
+                 </p>
+             </center>
+       </div>
+</body>
+</html>
